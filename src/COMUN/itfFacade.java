dc7420a2361b8fe0c1d1e24cac7395704b9cca8f
HodeiOA <HodeiOA@opendeusto.es>
2019-05-16 19:57:56 +0200

    itfFacade updated

--- src/COMUN/itfFacade.java
+++ src/COMUN/itfFacade.java
@@ -1,6 +1,21 @@
 package COMUN;
 
-public interface itfFacade
+import java.rmi.Remote;
+import java.rmi.RemoteException;
+import java.util.Date;
+import java.util.List;
+
+import ObjetosDominio.clsAeropuertoDTO;
+import ObjetosDominio.clsUsuarioDTO;
+import ObjetosDominio.clsVueloDTO;
+
+
+public interface itfFacade extends Remote
 {
-	
+	boolean RegistrarUsuario (clsUsuarioDTO nuevoUsuario, boolean modo)  throws RemoteException;
+	clsUsuarioDTO LoginUsuario (clsUsuarioDTO nuevoUsuario, boolean modo)  throws RemoteException;
+	public List<clsVueloDTO> BuscarVueloIda (clsAeropuertoDTO aeropuesrtoPred, String fecha, String ciudadOrigen, String ciudadDestino)  throws RemoteException;
+	public List<clsVueloDTO>BuscarVueloIdayVuelta (clsAeropuertoDTO aeropuesrtoPred, String fechaIda, String fechaVuelta, String ciudadOrigen, String ciudadDestino)  throws RemoteException;
+	public List<clsVueloDTO>BuscarVueloCualquierMomento (clsAeropuertoDTO aeropuesrtoPred, String ciudadOrigen, String ciudadDestino)  throws RemoteException;
+	boolean RealizarPagoyReserva (clsUsuarioDTO usuario, clsVueloDTO vuelo, int numAsiento, String nomViajero, boolean pasarela)  throws RemoteException;
 }
